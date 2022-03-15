5f08f80f5886598ef540ee975c50309380c4ab8a
nicktwoshoes <nmsalvatore@gmail.com>
2022-03-15 08:42:31 -0700

    Added admin route

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.status(200).render('index')
+})
+
+app.get('/admin', (req, res) => {
+    res.status(200).render('login')
 })
 
 app.listen(5000);
