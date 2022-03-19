580e7d8a65078472a5d06e84b0710e1a92251be4
nicktwoshoes <nmsalvatore@gmail.com>
2022-03-18 20:49:40 -0700

    Imported home and admin routers

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const homeRouter = require('./routes/index');
+const adminRouter = require('./routes/admin');
 const app = express();
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-
-app.get('/', (req, res) => {
-    res.status(200).render('index')
-})
-
-app.get('/admin', (req, res) => {
-    res.status(200).render('login')
-})
+app.use(express.urlencoded({ extended: false }))
+app.use('/', homeRouter);
+app.use('/admin', adminRouter)
 
 app.listen(5000);
